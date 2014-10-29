1fbf500e2bf8cec03e8a42424e5691fa707ca783
brianjscoles <brianscoles@gmail.com>
2014-10-29 16:32:53 -0700

    Completed Stack and completed first queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,33 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var front = 0;
+  var back = 0;
+  var count = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[back] = value;
+    back++;
+    count++;
   };
 
   someInstance.dequeue = function(){
+    if(count > 0){
+      var temp = storage[front];
+      count--;
+      front++;
+      delete temp;
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
 };
+
+
+
