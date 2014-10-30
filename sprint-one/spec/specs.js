b58f47b114620eea12a23dd03ba716c9192e7080
brianjscoles <brianscoles@gmail.com>
2014-10-30 11:41:12 -0700

    comment out all specs except megaArray

--- sprint-one/spec/specs.js
+++ sprint-one/spec/specs.js
@@ -42,27 +42,27 @@ define([
 
     describe('stack shared behavior', function(){
 
-      verifyClass(instantiator).followsPattern(variant, {}, prototypeOfInstances);
+      // verifyClass(instantiator).followsPattern(variant, {}, prototypeOfInstances);
 
-      it('reports a size of zero for a new stack', function() {
-        expect(stack.size()).to.equal(0);
-      });
+      // it('reports a size of zero for a new stack', function() {
+      //   expect(stack.size()).to.equal(0);
+      // });
 
-      it('reports a size of 2 after adding two items', function() {
-        stack.push('a');
-        stack.push('b');
-        expect(stack.size()).to.equal(2);
-      });
+      // it('reports a size of 2 after adding two items', function() {
+      //   stack.push('a');
+      //   stack.push('b');
+      //   expect(stack.size()).to.equal(2);
+      // });
 
-      it('reports a size of 6 after adding six items', function() {
-        stack.push('a');
-        stack.push('b');
-        stack.push('a');
-        stack.push('b');
-        stack.push('a');
-        stack.push('b');
-        expect(stack.size()).to.equal(6);
-      });
+      // it('reports a size of 6 after adding six items', function() {
+      //   stack.push('a');
+      //   stack.push('b');
+      //   stack.push('a');
+      //   stack.push('b');
+      //   stack.push('a');
+      //   stack.push('b');
+      //   expect(stack.size()).to.equal(6);
+      // });
 
      it('pushes and pops the whole megaArray successfully', function() {
 
@@ -78,47 +78,47 @@ define([
 
 
 
-      it('does not error when removing from an empty stack', function() {
-        expect(function(){ stack.pop(); }).not.throws();
-      });
+      // it('does not error when removing from an empty stack', function() {
+      //   expect(function(){ stack.pop(); }).not.throws();
+      // });
 
-      it('reports a size of 1 after adding two items and removing one', function() {
-        stack.push('a');
-        stack.push('b');
-        stack.pop();
-        expect(stack.size()).to.equal(1);
-      });
+      // it('reports a size of 1 after adding two items and removing one', function() {
+      //   stack.push('a');
+      //   stack.push('b');
+      //   stack.pop();
+      //   expect(stack.size()).to.equal(1);
+      // });
 
-      it('reports a size of 0 after removing more items than were added', function() {
-        stack.push('a');
-        stack.pop();
-        stack.pop();
-        expect(stack.size()).to.equal(0);
-      });
+      // it('reports a size of 0 after removing more items than were added', function() {
+      //   stack.push('a');
+      //   stack.pop();
+      //   stack.pop();
+      //   expect(stack.size()).to.equal(0);
+      // });
 
-      it('allows sequentially additing and removing items', function() {
-        stack.push('a');
-        expect(stack.pop()).to.equal('a');
-        stack.push('b');
-        expect(stack.pop()).to.equal('b');
-      });
+      // it('allows sequentially additing and removing items', function() {
+      //   stack.push('a');
+      //   expect(stack.pop()).to.equal('a');
+      //   stack.push('b');
+      //   expect(stack.pop()).to.equal('b');
+      // });
 
     });
 
     describe('stack-specific behavior', function(){
-      it('removes the most recently added of two items', function() {
-        stack.push('a');
-        stack.push('b');
-        expect(stack.pop()).to.equal('b');
-      });
+      // it('removes the most recently added of two items', function() {
+      //   stack.push('a');
+      //   stack.push('b');
+      //   expect(stack.pop()).to.equal('b');
+      // });
 
-      it('removes the newest item, after newer items have already been added and removed', function() {
-        stack.push('a');
-        stack.push('b');
-        stack.push('c');
-        stack.pop();
-        expect(stack.pop()).to.equal('b');
-      });
+      // it('removes the newest item, after newer items have already been added and removed', function() {
+      //   stack.push('a');
+      //   stack.push('b');
+      //   stack.push('c');
+      //   stack.pop();
+      //   expect(stack.pop()).to.equal('b');
+      // });
     });
 
   });
@@ -138,59 +138,59 @@ define([
 
     describe('queue shared behavior', function(){
 
-      verifyClass(instantiator).followsPattern(variant, {}, prototypeOfInstances);
+      // verifyClass(instantiator).followsPattern(variant, {}, prototypeOfInstances);
 
-      it('reports a size of zero for a new queue', function() {
-        expect(queue.size()).to.equal(0);
-      });
+      // it('reports a size of zero for a new queue', function() {
+      //   expect(queue.size()).to.equal(0);
+      // });
 
-      it('reports a size of 2 after adding two items', function() {
-        queue.enqueue('a');
-        queue.enqueue('b');
-        expect(queue.size()).to.equal(2);
-      });
+      // it('reports a size of 2 after adding two items', function() {
+      //   queue.enqueue('a');
+      //   queue.enqueue('b');
+      //   expect(queue.size()).to.equal(2);
+      // });
 
-      it('does not error when removing from an empty queue', function() {
-        expect(function(){ queue.dequeue(); }).not.throws();
-      });
+      // it('does not error when removing from an empty queue', function() {
+      //   expect(function(){ queue.dequeue(); }).not.throws();
+      // });
 
-      it('reports a size of 1 after adding two items and removing one', function() {
-        queue.enqueue('a');
-        queue.enqueue('b');
-        queue.dequeue();
-        expect(queue.size()).to.equal(1);
-      });
+      // it('reports a size of 1 after adding two items and removing one', function() {
+      //   queue.enqueue('a');
+      //   queue.enqueue('b');
+      //   queue.dequeue();
+      //   expect(queue.size()).to.equal(1);
+      // });
 
-      it('reports a size of 0 after removing more items than were added', function() {
-        queue.enqueue('a');
-        queue.dequeue();
-        queue.dequeue();
-        expect(queue.size()).to.equal(0);
-      });
+      // it('reports a size of 0 after removing more items than were added', function() {
+      //   queue.enqueue('a');
+      //   queue.dequeue();
+      //   queue.dequeue();
+      //   expect(queue.size()).to.equal(0);
+      // });
 
-      it('allows sequentially adding and removing items', function() {
-        queue.enqueue('a');
-        expect(queue.dequeue()).to.equal('a');
-        queue.enqueue('b');
-        expect(queue.dequeue()).to.equal('b');
-      });
+      // it('allows sequentially adding and removing items', function() {
+      //   queue.enqueue('a');
+      //   expect(queue.dequeue()).to.equal('a');
+      //   queue.enqueue('b');
+      //   expect(queue.dequeue()).to.equal('b');
+      // });
 
     });
 
     describe('queue-specific behavior', function(){
-      it('removes the least recently added of two items', function() {
-        queue.enqueue('a');
-        queue.enqueue('b');
-        expect(queue.dequeue()).to.equal('a');
-      });
+      // it('removes the least recently added of two items', function() {
+      //   queue.enqueue('a');
+      //   queue.enqueue('b');
+      //   expect(queue.dequeue()).to.equal('a');
+      // });
 
-      it('removes the oldest item, after newer items have already been added and removed', function() {
-        queue.enqueue('a');
-        queue.enqueue('b');
-        queue.dequeue();
-        queue.enqueue('c');
-        expect(queue.dequeue()).to.equal('b');
-      });
+      // it('removes the oldest item, after newer items have already been added and removed', function() {
+      //   queue.enqueue('a');
+      //   queue.enqueue('b');
+      //   queue.dequeue();
+      //   queue.enqueue('c');
+      //   expect(queue.dequeue()).to.equal('b');
+      // });
     });
 
   });
