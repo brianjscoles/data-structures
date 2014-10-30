0e162b79cca6e7b62b14edea5c2cadc0231d27a7
brianjscoles <brianscoles@gmail.com>
2014-10-30 16:37:59 -0700

    Completed linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,40 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    if(list.tail){
+      list.tail.next = newNode;
+    };
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while(true){
+      if(currentNode.value === target){
+        return true;
+      }
+      if(currentNode.next === null){
+        return false;
+      }
+      currentNode = currentNode.next;
+    }
   };
 
   return list;
 };
+
+
+
 
 var makeNode = function(value){
   var node = {};
