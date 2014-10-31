6eae62b955de13b22958aa50d50d4dd6e1ce4c8d
brianjscoles <brianscoles@gmail.com>
2014-10-30 17:52:46 -0700

    implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+    this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage[item]){
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  if(this.contains(item)){
+    delete this._storage[item];
+  }
 };
 
 /*
