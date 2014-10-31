320d3b67cb8d8e658ec3e6836a9b5dec4e8c71a9
brianjscoles <brianscoles@gmail.com>
2014-10-31 12:05:31 -0700

    Completed binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,63 @@
 var makeBinarySearchTree = function(value){
+  var bst = Object.create(bstMethods);
+  bst.left = null;
+  bst.right = null;
+  bst.value = value;
+  return bst;
+};
+
+var bstMethods = {};
+
+// bstMethods.bothBranches = function(callback){
+//   if(this.left){
+//     callback(this.left);
+//   }
+//   if(this.right){
+//     callback(this.right);
+//   }
+// };
+
+bstMethods.insert = function(value){
+  if(value > this.value){
+    if(this.right === null){
+      this.right = makeBinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  } else {
+    if(this.left === null){
+      this.left = makeBinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  }
+};
+
+bstMethods.contains = function(value){
+  if(this.value === value){
+    return true;
+  }
+  if(this.left){
+    if(this.left.contains(value)){
+      return true;
+    };
+  }
+  if(this.right){
+    if(this.right.contains(value)){
+      return true;
+    }
+  }
+  return false;
+};
+
+bstMethods.depthFirstLog = function(callback){
+  callback(this.value);
+  if(this.left){
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right){
+    this.right.depthFirstLog(callback);
+  }
 };
 
 
