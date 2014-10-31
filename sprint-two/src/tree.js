5dc27017b4b3ddf8ed51defdcc38ca6e63b26c24
brianjscoles <brianscoles@gmail.com>
2014-10-30 20:11:01 -0700

    Completed tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,7 +1,8 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  extensify(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,14 +12,32 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+   this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if(this.value === target){
+    return true;
+  }
+  for(var i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(target)){
+      return true;
+    }
+  }
+  return false;
 };
 
+var extensify = function(obj){
+  var args = Array.prototype.slice.call(arguments,1);
+  for(var i = 0; i < args.length; i++){
+    for(var key in args[i]){
+      obj[key] = args[i][key];
+    }
+  }
+  return obj;
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
