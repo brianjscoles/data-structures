2aafcc05576f202789cff42ac63c49ddd69a53b9
brianjscoles <brianscoles@gmail.com>
2014-10-31 14:28:54 -0700

    Started graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,13 +1,31 @@
 var Graph = function(){
+  this.nodes = [];
+  this.edges = [];
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+  this.nodes.push(newNode);
+  if(toNode){
+    this.edges.push([newNode, toNode]);
+  }
+  if(this.nodes.length === 2){
+    this.edges.push([this.nodes[0], this.nodes[1]]);
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return this.nodes.indexOf(node) >= 0;
 };
 
 Graph.prototype.removeNode = function(node){
+  if(this.nodes.indexOf(node) >= 0){
+    for(var i = 0; i < this.edges.length; i++){
+      if(this.edges[i].indexOf(node) >= 0){
+        this.edges.splice(i, 1);
+      }
+    }
+   this.nodes.splice(this.nodes.indexOf(node), 1);
+  };
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
