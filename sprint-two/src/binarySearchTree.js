2f775932a819595b10a1233c1355c62c6dbc93ef
brianjscoles <brianscoles@gmail.com>
2014-10-31 19:08:40 -0700

    Completed breadthFirstLog extracredit

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -6,16 +6,38 @@ var makeBinarySearchTree = function(value){
   return bst;
 };
 
-var bstMethods = {};
+var makeQueue = function() {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.front = 0;
+  queue.back = 0;
+  return queue;
 
-// bstMethods.bothBranches = function(callback){
-//   if(this.left){
-//     callback(this.left);
-//   }
-//   if(this.right){
-//     callback(this.right);
-//   }
-// };
+};
+
+var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this[this.back] = value;
+  this.back++;
+};
+
+queueMethods.dequeue = function() {
+  if(this.size()){
+    var temp = this[this.front];
+    this.front++;
+    delete temp;
+    return temp;
+  }
+};
+
+queueMethods.size = function() {
+  return this.back - this.front;
+};
+
+
+var bstMethods = {};
 
 bstMethods.insert = function(value){
   if(value > this.value){
@@ -60,7 +82,22 @@ bstMethods.depthFirstLog = function(callback){
   }
 };
 
+bstMethods.breadthFirstLog = function(callback){
+  var myQueue = makeQueue();
+  myQueue.enqueue(this);
+  while(myQueue.size() > 0){
+    currentNode = myQueue.dequeue();
+    callback(currentNode.value);
+    if(currentNode.left){
+      myQueue.enqueue(currentNode.left);
+    }
+    if(currentNode.right){
+      myQueue.enqueue(currentNode.right);
+    }
+  }
 
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
